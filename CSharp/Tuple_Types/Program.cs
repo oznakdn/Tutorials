e603a6329dbe1f6fdab6d92dbbee4856b835f613
ozan lütfü akaydın <ozanakaydin@gmail.com>
2022-09-19 21:26:48 +0300

    Tuple types added

---
+++ CSharp/Tuple_Types/Program.cs
@@ -0,0 +1,9 @@
+﻿/*********** TUPLE ***********/
+/*
+    - C# Tuple<string, bool, int>() şekinde tanımlanan metot geriye item1, item2, item3 olarak döner.
+ */
+
+using System.Data.SqlTypes;
+
+Tuple <int,string,bool> person = new Tuple<int, string, bool> (1,"Ahmet",true);
+Console.WriteLine($"ID : {person.Item1} Name: {person.Item2} Gender: {person.Item3}");
