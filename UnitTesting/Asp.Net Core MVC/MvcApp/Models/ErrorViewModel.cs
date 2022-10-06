9a1e53d5915cb4cb94e710931ba802a9cc103d2b
Ozan Akaydin <ozanakaydin@gmail.com>
2022-10-06 19:43:12 +0300

    updated

--- UnitTesting/Asp.Net Core MVC/MvcApp/Models/ErrorViewModel.cs
+++ UnitTesting/Asp.Net Core MVC/MvcApp/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace MvcApp.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
