af0397e1802ee6495ae5114739f632855eb22882
Ozan Akaydin <ozanakaydin@gmail.com>
2022-10-12 20:13:17 +0300

    EF Core Shadow Property

---
+++ EntityFrameworkCoreTutorial/08-ShadowProperty/Program.cs
@@ -0,0 +1,24 @@
+﻿
+/* 
+    * Tabloda gorunmesi istenmeyen yada islem yapilmayacak alanlar icin kullanilirlar.
+
+*/
+
+#region ForeignKey alanlarinda kullanimi
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string EmployeeName { get; set; }
+
+    // public int ProjectId { get; set; } alanini girmedigimizde EF Core arka planda kendi Shadow olarak olusturur.
+    public Project Project{get;set;}
+}
+
+public class Project
+{
+    public int Id { get; set; }
+    public string ProjectTitle { get; set; }
+    public ICollection<Employee>Employees{get;set;}
+}
+#endregion
