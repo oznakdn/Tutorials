d8fdd2678414f6d3ec6c7da5b8330b7c504e3822
Ozan Akaydin <ozanakaydin@gmail.com>
2022-11-11 20:39:23 +0300

    Authorize configuration was done

--- Microservices/CourseApp/Services/Catalog/Services.Catalog/Program.cs
+++ Microservices/CourseApp/Services/Catalog/Services.Catalog/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 using Services.Catalog.Services;
 using Services.Catalog.Settings;
@@ -6,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
 builder.Services.AddSingleton<IDatabaseSettings>(sp =>
@@ -15,6 +20,16 @@ builder.Services.AddSingleton<IDatabaseSettings>(sp =>
 });
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICourseService, CourseService>();
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServiceUrl"];
+    options.Audience = "thisismyaudience";
+    options.RequireHttpsMetadata = false;
+});
 
 
 builder.Services.AddEndpointsApiExplorer();
@@ -29,6 +44,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
